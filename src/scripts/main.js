b0da54ef7dc795a9edff8b52233e0762ec00377c
Vitalii <vitaliy.doronin4@gmail.com>
2026-08-27 19:25:40 +0300

    Add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,59 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (clickEvent) => {
+  const th = clickEvent.target.closest('th');
+
+  if (!th) {
+    return;
+  }
+
+  const trItems = tbody.querySelectorAll('tr');
+  const trArr = [...trItems];
+
+  if (th.textContent === 'Name') {
+    const sortedArr = trArr.sort((a, b) =>
+      a.cells[0].textContent.localeCompare(b.cells[0].textContent),
+    );
+
+    sortedArr.forEach((item) => {
+      tbody.append(item);
+    });
+  }
+
+  if (th.textContent === 'Position') {
+    const sortedArr = trArr.sort((a, b) =>
+      a.cells[1].textContent.localeCompare(b.cells[1].textContent),
+    );
+
+    sortedArr.forEach((item) => {
+      tbody.append(item);
+    });
+  }
+
+  if (th.textContent === 'Age') {
+    const sortedArr = trArr.sort(
+      (a, b) =>
+        a.cells[2].textContent.toString() - b.cells[2].textContent.toString(),
+    );
+
+    sortedArr.forEach((item) => {
+      tbody.append(item);
+    });
+  }
+
+  if (th.textContent === 'Salary') {
+    const sortedArr = trArr.sort(
+      (a, b) =>
+        +a.cells[3].textContent.slice(1).split(',').join('') -
+        +b.cells[3].textContent.slice(1).split(',').join(''),
+    );
+
+    sortedArr.forEach((item) => {
+      tbody.append(item);
+    });
+  }
+});
